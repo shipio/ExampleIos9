4fe89c75eebab7718665faac3ad103c96df6436d
Sandeep Tamhankar <sandman@electric-cloud.com>
2015-06-10 14:42:59 -0700

    Initial Commit

---
+++ ExampleIos9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleIos9
+//
+//  Created by Sandeep Tamhankar on 6/10/15.
+//  Copyright © 2015 ShipIO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
